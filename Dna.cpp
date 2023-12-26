6f02d8031d3b05f7cf0facf822bdd2ab5e0fc479
Youssef Sadouk <129612577+youssefsadouk@users.noreply.github.com>
2023-12-26 22:02:48 +0100

    [OpenMP Paralleziation] Fix the segmentation Fault

--- Dna.cpp
+++ Dna.cpp
@@ -7,14 +7,11 @@
 #include <cassert>
 #include <boost/dynamic_bitset.hpp>
 #include <sstream>
-#include <omp.h>
-
 
 Dna::Dna(int length, Threefry::Gen &&rng) {
     if (!shared_segment_) {
         // Create a new, independent sequence if no shared segment is being used
         seq_.resize(length);
-        #pragma omp parallel for
         for (int32_t i = 0; i < length; i++) {
             seq_[i] = rng.random(NB_BASE) != 0; // Randomly generate DNA sequence
         }
@@ -34,24 +31,17 @@ int Dna::length() const {
     }
 }
 
-
 void Dna::save(gzFile backup_file) {
     std::stringstream bitset_stream;
     int dna_length;
 
     if (shared_segment_) {
-        
-        #pragma omp parallel for ordered
         for (int i = segment_start_; i < segment_end_; ++i) {
-            #pragma omp ordered
             bitset_stream << shared_segment_->at(i);
         }
         dna_length = segment_end_ - segment_start_;
     } else {
-        
-        #pragma omp parallel for ordered
         for (size_t i = 0; i < seq_.size(); ++i) {
-            #pragma omp ordered
             bitset_stream << seq_[i];
         }
         dna_length = seq_.size();
@@ -70,24 +60,20 @@ void Dna::load(gzFile backup_file) {
 
     if (shared_segment_) {
         shared_segment_->lock();
-        
-        #pragma omp parallel for
         for (int i = 0; i < dna_length; ++i) {
             if (i + segment_start_ < shared_segment_->full_length()) {
                 shared_segment_->set(i + segment_start_, bitset_string[i] == '1');
+            } else {
+                // Error handling: if the DNA data exceeds the bounds of the shared segment
+                std::cerr << "Error: DNA data exceeds bounds of the shared segment." << std::endl;
+                break;
             }
         }
         shared_segment_->unlock();
     } else {
         seq_.clear();
-        
-        #pragma omp parallel for
-        for (int i = 0; i < dna_length; ++i) {
-            bool bit = bitset_string[i] == '1';
-            #pragma omp critical
-            {
-                seq_.push_back(bit);
-            }
+        for (char c : bitset_string) {
+            seq_.push_back(c == '1');
         }
     }
 }
@@ -143,22 +129,12 @@ void Dna::remove(int pos_1, int pos_2) {
             shared_segment_->unlock();
         }
     } else {
-            int num_bits_to_remove = pos_2 - pos_1;
-            int new_size = seq_.size() - num_bits_to_remove;
-            boost::dynamic_bitset<> new_seq(new_size);
-            
-            #pragma omp parallel for
-            for (int i = 0; i < pos_1; ++i) {
-                new_seq[i] = seq_[i];
-            }
-    
-            #pragma omp parallel for
-            for (int i = pos_2; i < seq_.size(); ++i) {
-                new_seq[i - num_bits_to_remove] = seq_[i];
-            }
-            
-            seq_ = std::move(new_seq);
-     }
+        int num_bits_to_remove = pos_2 - pos_1;
+        for (int i = pos_2; i < seq_.size(); ++i) {
+            seq_[i - num_bits_to_remove] = seq_[i];
+        }
+        seq_.resize(seq_.size() - num_bits_to_remove);
+    }
 }
 
 /**
@@ -183,29 +159,16 @@ void Dna::insert(int pos, std::vector<char> seq) {
 
         shared_segment_->unlock();
     } else {
-            int old_size = seq_.size();
-            int insert_size = seq.size();
-            int new_size = old_size + insert_size;
-            boost::dynamic_bitset<> new_seq(new_size);
-    
-            #pragma omp parallel for
-            for (int i = 0; i < pos; ++i) {
-                new_seq[i] = seq_[i];
-            }
-    
-            #pragma omp parallel for
-            for (size_t i = 0; i < insert_size; ++i) {
-                new_seq[pos + i] = seq[i] == '1';
-            }
-    
-            #pragma omp parallel for
-            for (int i = pos; i < old_size; ++i) {
-                new_seq[i + insert_size] = seq_[i];
-            }
-            
-            seq_ = std::move(new_seq);
+        seq_.resize(seq_.size() + seq.size());
+        for (int i = seq_.size() - 1; i >= pos + seq.size(); --i) {
+            seq_[i] = seq_[i - seq.size()];
         }
+        for (size_t i = 0; i < seq.size(); ++i) {
+            seq_[pos + i] = seq[i] == '1';
+        }
+    }
 }
+
 
 /**
  * Insert a sequence of a given length at a given position into the DNA of the Organism
@@ -236,31 +199,15 @@ void Dna::insert(int pos, Dna* seq) {
 
         shared_segment_->unlock();
     } else {
-            int old_size = seq_.size();
-            int insert_size = seq->seq_.size();
-            int new_size = old_size + insert_size;
-            boost::dynamic_bitset<> new_seq(new_size);
-    
-            // Copy bits before the insertion point
-            #pragma omp parallel for
-            for (int i = 0; i < pos; ++i) {
-                new_seq[i] = seq_[i];
-            }
-    
-            // Insert the new sequence
-            #pragma omp parallel for
-            for (size_t i = 0; i < insert_size; ++i) {
-                new_seq[pos + i] = seq->seq_[i];
-            }
-    
-            // Shift the rest of the bits to make room for the new sequence
-            #pragma omp parallel for
-            for (int i = pos; i < old_size; ++i) {
-                new_seq[i + insert_size] = seq_[i];
-            }
-    
-            seq_ = std::move(new_seq);
+        // Original code for non-shared segment
+        seq_.resize(seq_.size() + seq->seq_.size());
+        for (int i = seq_.size() - 1; i >= pos + seq->seq_.size(); --i) {
+            seq_[i] = seq_[i - seq->seq_.size()];
         }
+        for (size_t i = 0; i < seq->seq_.size(); ++i) {
+            seq_[pos + i] = seq->seq_[i];
+        }
+    }
 }
 
 
@@ -304,34 +251,39 @@ void Dna::do_duplication(int pos_1, int pos_2, int pos_3) {
 
         shared_segment_->unlock();
     } else {
-        size_t dupl_length = (pos_1 < pos_2) ? pos_2 - pos_1 : seq_.size() - pos_1 + pos_2;
-        size_t new_size = seq_.size() + dupl_length;
-        boost::dynamic_bitset<> new_seq(new_size);
+        boost::dynamic_bitset<> seq_dupl;
+		size_t dupl_length = (pos_1 < pos_2) ? pos_2 - pos_1 : seq_.size() - pos_1 + pos_2;
+		seq_dupl.resize(dupl_length);
 
-        // Parallel region starts
-        #pragma omp parallel
-        {
-            // Fill the duplicated segment
-            #pragma omp for nowait
-            for (size_t i = 0; i < dupl_length; ++i) {
-                size_t index = (pos_1 + i) % seq_.size(); // for circular duplication
-                new_seq[pos_3 + i] = seq_[index];
-            }
+		// Fill the duplicated segment bit by bit
+		if (pos_1 < pos_2) {
+			for (size_t i = 0; i < dupl_length; ++i) {
+			    seq_dupl[i] = seq_[pos_1 + i];
+			}
+		} else {
+			size_t i = 0;
+			// First part from pos_1 to end
+			for (; pos_1 + i < seq_.size(); ++i) {
+			    seq_dupl[i] = seq_[pos_1 + i];
+			}
+			// Second part from start to pos_2
+			for (size_t j = 0; j < pos_2; ++j, ++i) {
+			    seq_dupl[i] = seq_[j];
+			}
+		}
 
-            // Copy the original sequence until the duplication point
-            #pragma omp for nowait
-            for (size_t i = 0; i < pos_3; ++i) {
-                new_seq[i] = seq_[i];
-            }
+		// Resize the original bitset to accommodate the duplicated segment
+		seq_.resize(seq_.size() + dupl_length);
 
-            // Shift bits after pos_3 to make room for the duplicate segment
-            #pragma omp for
-            for (size_t i = pos_3; i < seq_.size(); ++i) {
-                new_seq[i + dupl_length] = seq_[i];
-            }
-        } // Parallel region ends
+		// Shift bits after pos_3 to make room for the duplicate segment
+		for (int i = seq_.size() - dupl_length - 1; i >= pos_3; --i) {
+			seq_[i + dupl_length] = seq_[i];
+		}
 
-        seq_ = std::move(new_seq);
+		// Insert the duplicated segment
+		for (size_t i = 0; i < dupl_length; ++i) {
+			seq_[pos_3 + i] = seq_dupl[i];
+		}
     }
 }
 
@@ -341,23 +293,26 @@ int Dna::promoter_at(int pos) {
 
     int dist_lead = 0;
 
-    #pragma omp parallel for reduction(+:dist_lead)
     for (int motif_id = 0; motif_id < PROM_SIZE; motif_id++) {
+        // Ensure we don't go past the end of the sequence
         int curr_pos = pos + motif_id;
-        if (shared_segment_ ? curr_pos >= shared_segment_->full_length() : curr_pos >= seq_.size()) {
-            #pragma omp cancel for
-        }
+        if (shared_segment_ ? curr_pos >= shared_segment_->full_length() : curr_pos >= seq_.size()) break;
 
+        // Convert character in PROM_SEQ to a boolean
         bool expected_bit = PROM_SEQ[motif_id] == '1';
+
+        // Access the bit either from shared_segment or from seq_
         bool actual_bit = shared_segment_ ? shared_segment_->at(curr_pos) == '1' : seq_[curr_pos];
 
         if (actual_bit != expected_bit) {
-            dist_lead++;
+            dist_lead++;  
         }
     }
 
     return dist_lead;
 }
+
+
 
 // Given a, b, c, d boolean variable and X random boolean variable,
 // a terminator look like : a b c d X X !d !c !b !a
@@ -366,87 +321,87 @@ int Dna::terminator_at(int pos) {
 
     int dist_term_lead = 0;
 
-    #pragma omp parallel for reduction(+:dist_term_lead)
     for (int motif_id = 0; motif_id < TERM_STEM_SIZE; motif_id++) {
         int right = pos + motif_id;
         int left = pos + (TERM_SIZE - 1) - motif_id;
 
-        // Check if 'right' or 'left' goes beyond the sequence length
-        if (shared_segment_ ? (right >= shared_segment_->full_length() || left >= shared_segment_->full_length()) : (right >= seq_.size() || left >= seq_.size())) {
-            continue; // Skip this iteration
-        }
+        // If either 'right' or 'left' goes beyond the sequence length, stop checking
+        if (shared_segment_ ? (right >= shared_segment_->full_length() || left >= shared_segment_->full_length()) : (right >= seq_.size() || left >= seq_.size())) break;
 
+        // Check if the bits at 'right' and 'left' are different
         bool right_bit = shared_segment_ ? shared_segment_->at(right) == '1' : seq_[right];
         bool left_bit = shared_segment_ ? shared_segment_->at(left) == '1' : seq_[left];
 
         if (right_bit != left_bit) {
-            dist_term_lead++;
+            dist_term_lead++; 
         }
     }
 
     return dist_term_lead;
 }
 
+
 bool Dna::shine_dal_start(int pos) {
     assert(pos >= 0 && (shared_segment_ ? pos < shared_segment_->full_length() : pos < seq_.size()));
 
-    bool result = true;
-    #pragma omp parallel for reduction(&&:result)
     for (int k = 0; k < SHINE_DAL_SIZE + CODON_SIZE; k++) {
         int k_t = k >= SHINE_DAL_SIZE ? k + SD_START_SPACER : k;
         int t_pos = pos + k_t;
 
-        if (shared_segment_ ? t_pos >= shared_segment_->full_length() : t_pos >= seq_.size()) {
-            result = false;
-            continue;
-        }
-
+        if (shared_segment_ ? t_pos >= shared_segment_->full_length() : t_pos >= seq_.size()) return false;
+        
         char expected_char = SHINE_DAL_SEQ[k_t];
-        if (expected_char != '*' && shared_segment_->at(t_pos) != expected_char) {
-            result = false;
+
+        if (expected_char != '*') {  
+            bool expected_bit = expected_char == '1';
+
+            bool actual_bit = shared_segment_ ? shared_segment_->at(t_pos) == '1' : seq_[t_pos];
+
+            if (actual_bit != expected_bit) {
+                return false;
+            }
         }
     }
-    return result;
+
+    return true;
 }
 
 bool Dna::protein_stop(int pos) {
     assert(pos >= 0 && (shared_segment_ ? pos < shared_segment_->full_length() : pos < seq_.size()));
 
-    bool result = true;
-    #pragma omp parallel for reduction(&&:result)
     for (int k = 0; k < CODON_SIZE; k++) {
         int t_k = pos + k;
 
         if (shared_segment_ ? t_k >= shared_segment_->full_length() : t_k >= seq_.size()) {
-            result = false;
-            continue;
+            return false;
         }
+        
+        bool expected_bit = PROTEIN_END[k] == '1';
 
-        if (shared_segment_->at(t_k) != PROTEIN_END[k]) {
-            result = false;
+        bool actual_bit = shared_segment_ ? shared_segment_->at(t_k) == '1' : seq_[t_k];
+
+        if (actual_bit != expected_bit) {
+            return false;
         }
     }
-    return result;
+
+    return true;
 }
 
 int Dna::codon_at(int pos) {
     assert(pos >= 0 && (shared_segment_ ? pos <= shared_segment_->full_length() - CODON_SIZE : pos <= seq_.size() - CODON_SIZE));
 
-    bool withinBounds = true;
-    #pragma omp parallel for reduction(&&:withinBounds)
-    for (int i = 0; i < CODON_SIZE; i++) {
-        int t_pos = pos + i;
-        if (shared_segment_ ? t_pos >= shared_segment_->full_length() : t_pos >= seq_.size()) {
-            withinBounds = false;
-        }
-    }
-
-    if (!withinBounds) return -1;
-
     int value = 0;
+
     for (int i = 0; i < CODON_SIZE; i++) {
         int t_pos = pos + i;
+
+        if (shared_segment_ ? t_pos >= shared_segment_->full_length() : t_pos >= seq_.size()) {
+            return -1;
+        }
+
         bool bit = shared_segment_ ? shared_segment_->at(t_pos) == '1' : seq_[t_pos];
+
         if (bit) {
             value |= 1 << (CODON_SIZE - i - 1);
         }
